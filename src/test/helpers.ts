c553bf3c184b8cf7787f7a3dfd522f7a46f92834
SuperPaintman <SuperPaintmanDeveloper@gmail.com>
2017-12-12 01:27:11 +0500

    feat(test): Added `promise` extention for `Chai` and handling unhandled rejection

--- src/test/helpers.ts
+++ src/test/helpers.ts
@@ -1,6 +1,37 @@
 'use strict';
+/** Imports */
 import 'source-map-support/register';
 
 process.env.NODE_ENV = 'test';
 
-export { expect } from 'chai';
+import { inspect } from 'util';
+
+import * as chai from 'chai';
+import * as chaiAsPromised from 'chai-as-promised';
+
+const { color } = require('mocha/lib/reporters/base');
+
+
+/** Init */
+let haveUnhandledRejection = false;
+process.on('unhandledRejection', (err, p) => {
+  haveUnhandledRejection = true;
+
+  console.error(color('fail', 'Unhandled Rejection: ' + inspect(err)));
+
+  throw err;
+});
+
+process.on('exit', (code) => {
+  if (code === 0 && haveUnhandledRejection) {
+    process.exit(1);
+  }
+});
+
+
+
+chai.use(chaiAsPromised);
+
+export const { expect } = chai;
+
+
